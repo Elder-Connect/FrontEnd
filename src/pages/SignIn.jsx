3e837938411395bbf33b7df2e6097c8e1e6290dd
DaniloPedrazzi <danilo.pedrazzi@sptech.school>
2024-04-03 20:48:11 -0300

    Minor Fixes SignUp

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -3,9 +3,12 @@ import Header from '../components/Header'
 import './Sign.css'
 import SignImage from '../assets/img/sign.svg'
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function SignIn() {
   const [formData, setFormData] = useState({ name: '', document: '', email: '', password: '' });
+
+  const navigate = useNavigate();
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -30,13 +33,16 @@ function SignIn() {
                 <img src={SignImage} alt="Ilustração Login" />
             </div>
             <div className="formContainer">
+              <div className="">
                 <h1 className='formTitle'>Login</h1>
                 <p className="formSubtitle">Entre para encontrar cuidadores prontos para você!</p>
-                <form onSubmit={handleFormSubmit}>
+              </div>
+              <form onSubmit={handleFormSubmit}>
                 <input type="email" name="email" value={formData.email} onChange={handleInputChange} placeholder="Email" />
                 <input type="password" name="password" value={formData.password} onChange={handleInputChange} placeholder="Senha" />
                 <button type="submit" className='btn'>Entrar</button>
-                </form>
+              </form>
+              <p className='formSubtitle' style={{marginTop: '1em'}}>Esqueceu sua Senha? <span style={{cursor: 'pointer', color: 'var(--primary)'}} onClick={() => navigate("/Cadastro")}>Recupere aqui</span></p>
             </div>
         </div>
       </div>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,9 +3,12 @@ import Header from '../components/Header'
 import './Sign.css'
 import SignImage from '../assets/img/sign.svg'
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function SignUp() {
   const [formData, setFormData] = useState({ name: '', document: '', email: '', password: '' });
+
+  const navigate = useNavigate();
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -49,15 +52,18 @@ function SignUp() {
                 <img src={SignImage} alt="Ilustração Cadastro" />
             </div>
             <div className="formContainer">
+              <div className="">
                 <h1 className='formTitle'>Cadastro</h1>
                 <p className="formSubtitle">Cadastre-se agora para encontrar cuidadores prontos para você!</p>
-                <form onSubmit={handleFormSubmit}>
+              </div>
+              <form onSubmit={handleFormSubmit}>
                 <input type="text" name="name" value={formData.name} onChange={handleInputChange} placeholder="Nome" />
                 <input type="text" name="document" value={formData.document} onChange={handleInputChange} placeholder="CPF/CNPJ" />
                 <input type="email" name="email" value={formData.email} onChange={handleInputChange} placeholder="Email" />
                 <input type="password" name="password" value={formData.password} onChange={handleInputChange} placeholder="Senha" />
                 <button type="submit" className='btn'>Cadastro</button>
-                </form>
+              </form>
+              <p className='formSubtitle' style={{marginTop: '1em'}}>Já tem uma conta? <span className='textLink' onClick={() => navigate("/Entrar")}>Faça Login</span></p>
             </div>
         </div>
       </div>
