b5592195fdc8736e61997d423a37dd95953efea5
DaniloPedrazzi <danilo.justino1@gmail.com>
2024-05-18 10:42:33 -0300

    Improve Auth

--- src/services/Authentication.jsx
+++ src/services/Authentication.jsx
@@ -4,9 +4,18 @@ import { UserContext } from "../App";
 
 const Authentication = ({ children }) => {
   const { user } = useContext(UserContext);
-  if (!user) {
+
+  if (user === undefined) {
+    //TODO Loading component
+    return <div style={{display: 'flex', placeContent: 'center', height: '50vh'}}>
+            <img src="https://thebowlcut.com/cdn/shop/t/41/assets/loading.gif?v=157493769327766696621701744369" alt="loading gif" />
+          </div>;
+  }
+
+  if (!user || user === null) {
     return <Navigate to="/" />;
   }
+
   return children;
 };
 
