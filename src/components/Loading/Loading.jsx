ea04eafc8e6292b7149956571d7ba14a57febef3
Bruno Vinicius <bruno.bizerra@sptech.school>
2024-05-29 18:59:50 -0300

    @BrunoViniciusB componente de loading

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Loading = () => (
+  <div style={{ display: 'flex', placeContent: 'center', height: '50vh' }}>
+    <img src="https://thebowlcut.com/cdn/shop/t/41/assets/loading.gif?v=157493769327766696621701744369" alt="loading gif" />
+  </div>
+);
+
+export default Loading;
