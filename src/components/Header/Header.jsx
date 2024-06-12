05edef1355d674b6b6be8b18a0aa59d1de1f5e8d
DaniloPedrazzi <danilo.justino1@gmail.com>
2024-06-12 10:52:11 -0300

    Fix Header not loading on first access

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { React, useContext } from 'react'
+import { React, useContext, useState } from 'react'
 import { useNavigate } from "react-router-dom"
 import './Header.css'
 import Logo from '../../assets/img/logo.svg'
@@ -6,10 +6,18 @@ import HeaderDropdown from './HeaderDropdown/HeaderDropdown'
 import GoogleBtn from './GoogleBtn/GoogleBtn';
 import { USERTYPE } from '../../services/enums';
 import { UserContext } from '../../App'
+import { useEffect } from 'react'
 
 function Header() {
   const navigate = useNavigate();
   const { user } = useContext(UserContext);
+  const [isAdm, setIsAdm] = useState(false);
+
+  useEffect(() => {
+    if(localStorage.getItem('userType') == USERTYPE.ADM) {
+      setIsAdm(true);
+    }
+  }, [user])
   
   return (
     <header>
@@ -18,7 +26,7 @@ function Header() {
           <button className='btnNoBg' onClick={() => navigate("/")}>Home</button>
           <button className='btnNoBg' onClick={() => navigate("/Cuidadores")}>Cuidadores</button>
           {user ? <button className='btnNoBg' onClick={() => navigate("/Chat")}>Chat</button> : null}
-          {user && localStorage.getItem("userType") == USERTYPE.ADM ? <><button className='btnNoBg' onClick={() => navigate("/CadastroCuidador")}>Cadastro de Cuidadores</button> <button className='btnNoBg' onClick={() => navigate("/RelatorioCuidador")}>Relatório</button></> : null}
+          {isAdm ? <><button className='btnNoBg' onClick={() => navigate("/CadastroCuidador")}>Cadastro de Cuidadores</button> <button className='btnNoBg' onClick={() => navigate("/RelatorioCuidador")}>Relatório</button></> : null}
         </nav>
         { user ? <HeaderDropdown /> : 
           <div className='user'>
