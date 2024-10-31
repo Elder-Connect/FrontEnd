e7231bd0182222681d20abba395ab092e88a6a46
Bruno Vinicius <bruno.bizerra@sptech.school>
2024-10-30 22:33:49 -0300

    Atualizações no componente pipefy

--- src/components/Pipefy/Pipefy.jsx
+++ src/components/Pipefy/Pipefy.jsx
@@ -4,10 +4,9 @@ import BotaoDenuncia from '../../assets/img/icon-denuncia.svg';
 
 const Pipefy = () => {
     return (
-        <a href="https://app.pipefy.com/organizations/301375919/portals?pipeId=304835112&pipeUuid=1b7f9d11-7851-4eb7-95db-ad44a76d9777" className="pipefy">
-            <img src={BotaoDenuncia} alt="Denúncia" />
+        <a href="https://app.pipefy.com/public/form/qEDdcd6i" className="pipefy" target="_blank">
+            <img src={BotaoDenuncia} alt="Denúncia" title="Denúncia"/>
         </a>
-
     )
 }
 
