79d1348c09db1f0a4b6a67b170caf7659428eb8d
DaniloPedrazzi <danilo.justino1@gmail.com>
2024-11-20 14:17:30 -0300

    Change "Denuncia" Button to "Suporte"

--- src/components/Pipefy/Pipefy.jsx
+++ src/components/Pipefy/Pipefy.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import './Pipefy.css';
-import BotaoDenuncia from '../../assets/img/icon-denuncia.svg';
+import BotaoSuporte from '../../assets/img/icon-suporte.svg';
 
 const Pipefy = () => {
     return (
         <a href="https://app.pipefy.com/public/form/qEDdcd6i" className="pipefy" target="_blank">
-            <img src={BotaoDenuncia} alt="Denúncia" title="Denúncia"/>
+            <img src={BotaoSuporte} alt="Suporte" title="Suporte"/>
         </a>
     )
 }
